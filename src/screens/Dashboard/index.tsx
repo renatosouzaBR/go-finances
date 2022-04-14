dcdad22ea5dce0a30246ab4043d07adee0d6277f
renatosouzaBR <natoicm@gmail.com>
2022-04-14 09:56:34 -0300

    feat: add user info in header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,36 @@
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserProfile,
+  UserPhoto,
+  UserGreetings,
+  Greeting,
+  UserName,
+  ExitIcon,
+  UserWrapper,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserProfile>
+            <UserPhoto
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/28769727?v=4",
+              }}
+            />
+
+            <UserGreetings>
+              <Greeting>Olá,</Greeting>
+              <UserName>Renato</UserName>
+            </UserGreetings>
+          </UserProfile>
+
+          <ExitIcon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,57 @@
 import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
-
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 100%;
+  height: ${RFPercentage(34)}px;
+
+  justify-content: center;
+`;
+
+export const UserWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 24px;
+`;
+
+export const UserProfile = styled.View`
+  flex-direction: row;
+`;
+
+export const UserPhoto = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+export const UserGreetings = styled.View`
+  margin-left: ${RFValue(18)}px;
+`;
+
+export const Greeting = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  line-height: ${RFValue(24)}px;
+`;
+
+export const UserName = styled.Text`
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.title};
+  color: ${({ theme }) => theme.colors.shape};
+  line-height: ${RFValue(24)}px;
+`;
+
+export const ExitIcon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
