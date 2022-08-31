17e94702ec0dcd5f9164a2ab5278017e8d426b31
renatosouzaBR <natoicm@gmail.com>
2022-08-31 19:05:04 -0300

    test: ensure user not be loadded when Google sign in is canceled

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,24 +1,29 @@
 import { renderHook, act } from "@testing-library/react-hooks";
+import * as AuthSession from "expo-auth-session";
 
 import { AuthProvider, useAuth } from "./auth";
 
-jest.mock("expo-auth-session", () => ({
-  startAsync: () => ({
-    type: "success",
-    params: {
-      access_token: "google_token",
-    },
-  }),
-}));
-
 describe("Auth hook", () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+  });
+
   it("should be able to sign in with Google account", async () => {
+    jest.spyOn(AuthSession, "startAsync").mockResolvedValue({
+      type: "success",
+      params: {
+        access_token: "google_token",
+      },
+    } as any);
+
     const fetchJson = jest.fn().mockResolvedValue({
       id: "id",
       given_name: "name",
       email: "email",
       picture: "picture",
     });
+
     global.fetch = jest.fn().mockResolvedValue({
       json: fetchJson,
     });
@@ -28,5 +33,16 @@ describe("Auth hook", () => {
     await act(() => result.current.signInWithGoogle());
 
     expect(result.current.user).toBeTruthy();
+  });
+
+  it("user should not be loaded when Google sign in is canceled", async () => {
+    jest.spyOn(AuthSession, "startAsync").mockResolvedValue({
+      type: "cancel",
+    } as any);
+    const { result } = renderHook(() => useAuth(), { wrapper: AuthProvider });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty("id");
   });
 });
